a9b4f9f6be64f6c52b4d3314764aa702b087da88
Chris Moss <hcmoss70@gmail.com>
2022-11-08 21:40:20 -0500

    added react root and router

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+
+import Router from "./router"
+
+
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+              <Router />
+  </React.StrictMode>
+)
---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { Navigate, Route, Routes, useLocation } from "react-router-dom"
+
+
+import { ReactElement } from "react"
+
+
+
+export const Router = () => {
+  return (
+    <Routes>
+      
+     
+        
+    </Routes>
+  )
+}
+
+export default Router
