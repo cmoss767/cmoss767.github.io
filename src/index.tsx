fafbd44ba2e5a2e88dc763b2f765ae4347b529a2
Chris Moss <hcmoss70@gmail.com>
2022-11-09 22:57:16 -0500

    added theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-
+import { ThemeProvider } from "@mui/material"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import main from "./themes/light"
 
 
 import Router from "./router"
@@ -14,9 +15,11 @@ import Router from "./router"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
+        <ThemeProvider theme={main}>
      <BrowserRouter>
              <Router/>
              </BrowserRouter>
+             </ThemeProvider>
             
     </React.StrictMode>
 )
