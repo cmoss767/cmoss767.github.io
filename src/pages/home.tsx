b85d9889dd2a8f07aacdd01ff509529ea4afebae
Chris Moss <hcmoss70@gmail.com>
2022-11-09 22:14:01 -0500

    added navbar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,49 @@
 
-import { Box,  Typography } from "@mui/material"
+import { AppBar, Box, Button, IconButton,  Toolbar, Typography } from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
+import Brightness4Icon from '@mui/icons-material/Brightness4'
+import NightsStayIcon from '@mui/icons-material/NightsStay';
+
 
 
 
 
 const Home = () =>{
 return (
-  
-<Box>Hello there</Box>
-    
+    <>
+    <AppBar position="static" >
+    <Toolbar >
+        
+              <IconButton
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography  >
+                News
+              </Typography>
+              <Button >Login</Button>
+              <Box>
+              <IconButton
+                sx={{ mr: 2 }}
+              >
+                <Brightness4Icon />
+              </IconButton>
+              <IconButton
+                sx={{ mr: 2 }}
+              >
+                <NightsStayIcon />
+              </IconButton>
+              </Box>
+           
+            </Toolbar>
+    </AppBar>
+<Box>
+    <Typography>
+    Hello there! My name is Chris -- I am a software developer.
+    </Typography>
+    </Box>
+</>
 )
 }
 
