2fd86a0a4e7f18f3a8b7c3f978cc0f28aca7c84d
Chris Moss <hcmoss70@gmail.com>
2023-03-09 23:39:57 -0500

    changed refs

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -8,18 +8,18 @@ const Navbar = () => {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+          <a href="#about" className="mr-5 hover:text-white">
             About Me
           </a>
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
+          <a href="#projects" className="mr-5 hover:text-white">
             Projects
           </a>
         </nav>
         <a
-          href="#contact"
+          href="#resume"
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
         >
           Resume
