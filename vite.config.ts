c8c2254a397b237275a88a4b7cfdbf988fb48b56
Chris Moss <hcmoss70@gmail.com>
2023-03-21 21:36:30 -0400

    app build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  base: "/repo-name",
 
-    server: {
-        host: true,
-        port: 8000
-    }
+  server: {
+    host: true,
+    port: 8000,
+  },
 })
