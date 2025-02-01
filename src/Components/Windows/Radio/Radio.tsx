d08eabcbcdbdd9dbfcbd7caef135a5c2ecdcebfd
cmoss767 <hcmoss70@gmail.com>
2025-02-01 16:40:47 -0600

    fix radio height

--- src/Components/Windows/Radio/Radio.tsx
+++ src/Components/Windows/Radio/Radio.tsx
@@ -9,7 +9,7 @@ const AudioPlayer = () => {
           <h2 className="text-center font-semibold text-lg mb-4">Radio</h2>
           <div className="w-full px-4">
             <div className="bg-black border-3 border-black rounded-lg overflow-hidden">
-              <div className="h-[350px]">
+              <div className="h-[250px]">
                 <AudioVisualizer />
               </div>
             </div>
